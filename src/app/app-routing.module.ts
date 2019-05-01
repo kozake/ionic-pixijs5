d514d67cf0b8053c0906f2be81b5abea3348b9f0
kozake <s.kozale@gmail.com>
2019-05-01 22:18:09 +0900

    pageの整理

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'basic',
     pathMatch: 'full'
-  },
-  {
-    path: 'home',
-    loadChildren: './home/home.module#HomePageModule'
-  },
-  {
-    path: 'list',
-    loadChildren: './list/list.module#ListPageModule'
   },
   { path: 'basic', loadChildren: './pages/basic/basic.module#BasicPageModule' }
 ];
