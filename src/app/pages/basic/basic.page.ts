8e066caff5c977f4c6d4906278910d41e65b2c99
kozake <s.kozale@gmail.com>
2019-05-01 22:51:02 +0900

    Basic

--- src/app/pages/basic/basic.page.ts
+++ src/app/pages/basic/basic.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, NgZone } from '@angular/core';
+import * as PIXI from 'pixi.js';
 
 @Component({
   selector: 'app-basic',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicPage implements OnInit {
 
-  constructor() { }
+  @ViewChild('screen')
+  screen: ElementRef;
+
+  app: PIXI.Application;
+
+  constructor(private zone: NgZone) { }
 
   ngOnInit() {
+    this.zone.runOutsideAngular(() => this.appOnInit());
   }
 
+  appOnInit() {
+    this.app = new PIXI.Application({ backgroundColor: 0x1099bb });
+    this.screen.nativeElement.appendChild(this.app.view);
+
+    // create a new Sprite from an image path
+
+    const bunny = PIXI.Sprite.from('assets/bunny.png');
+    // center the sprite's anchor point
+    bunny.anchor.set(0.5);
+
+    // move the sprite to the center of the screen
+    bunny.x = this.app.screen.width / 2;
+    bunny.y = this.app.screen.height / 2;
+
+    this.app.stage.addChild(bunny);
+
+    // Listen for animate update
+    this.app.ticker.add((delta) => {
+        // just for fun, let's rotate mr rabbit a little
+        // delta is 1 if running at 100% performance
+        // creates frame-independent transformation
+        bunny.rotation += 0.1 * delta;
+    });
+  }
 }
